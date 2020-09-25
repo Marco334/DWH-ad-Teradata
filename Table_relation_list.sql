65f5a9fc23cbc63fc918f394f10471ed9ed806c0
Marco Genca <54363103+Marco334@users.noreply.github.com>
2020-09-26 00:41:22 +0200

    Create Table_relation_list.sql

---
+++ Table_relation_list.sql
@@ -0,0 +1,15 @@
+-- Quali tabelle hanno chiavi esterne e con quali altre ?
+-- Tables having FK belonging as PK to other tables (rleations )
+SELECT DISTINCT ChildDB || '.' || ChildTable AS ForeignTable
+               ,'>-' AS Rel
+               ,ParentDB || '.' || ParentTable AS PrimaryTable
+                 
+           FROM DBC.All_RI_ChildrenV
+		   
+          WHERE ChildDB NOT IN ('All', 'Crashdumps', 'DBC', 'dbcmngr', 
+              'Default', 'External_AP', 'EXTUSER', 'LockLogShredder', 'PUBLIC',
+              'Sys_Calendar', 'SysAdmin', 'SYSBAR', 'SYSJDBC', 'SYSLIB', 
+              'SystemFe', 'SYSUDTLIB', 'SYSUIF', 'TD_SERVER_DB', 'TDStats',
+              'TD_SYSGPL', 'TD_SYSXML', 'TDMaps', 'TDPUSER', 'TDQCD',
+              'tdwm', 'SQLJ', 'TD_SYSFNLIB', 'SYSSPATIAL')
+      ORDER BY 1,2;
